f4390444e00ac5815f52b3832ae106da7e0c497e
neolead <janvarry@gmail.com>
2020-10-21 00:14:33 +0300

    Add files via upload

---
+++ ap-server.ino
@@ -0,0 +1,101 @@
+/*
+  Skyrc IMAX B6 - uart to serial port over esp8266 server
+  Using for esp8266
+  Remark: If you got bad uart signal :
+  remove resistor R4 https://github.com/neolead/skyrc-imax-b6-usb-uart-over-wifi-with-esp8266/raw/main/uart.jpg
+  thanks to : https://github.com/stawel/cheali-charger/issues/256
+  Created by neolead: janvarry@gmail.com nov.2020
+*/
+
+#include <ESP8266WiFi.h>
+
+//how many clients should be able to telnet to this ESP8266
+#define MAX_SRV_CLIENTS 1
+#define TCP_PORT (23)           // Choose any port you want
+WiFiServer tcpServer(TCP_PORT);
+WiFiClient tcpServerClients[MAX_SRV_CLIENTS];
+
+IPAddress apIP(192, 168, 4, 1);
+const char SSID[] = "TCPUARTBridge";  // Choose any SSID
+//const char PASSWORD[] = "12345678"; // minimum 8 characters. !!!CHANGE THIS!!!
+const char PASSWORD[] = "12345678"; // minimum 8 characters. !!!CHANGE THIS!!!
+
+#define SerialDebug Serial1   // Debug goes out on GPIO02
+#define SerialGPS   Serial    // GPS or other device connected to the ESP UART
+
+#ifndef min
+#define min(x,y)  ((x)<(y)?(x):(y))
+#endif
+
+void setup() {
+  // !!! Debug output goes to GPIO02 !!!
+  SerialDebug.begin(9600);
+  SerialDebug.println("TCP <-> UART bridge");
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP(SSID, PASSWORD);
+
+  //start UART. Be sure to set the speed to match the speed of whatever is
+  //connected  to the UART.
+  SerialGPS.begin(9600);
+
+  // Start TCP listener on port TCP_PORT
+  tcpServer.begin();
+  tcpServer.setNoDelay(true);
+  SerialDebug.print("Ready! Use 'telnet or nc ");
+  SerialDebug.print(WiFi.localIP());
+  SerialDebug.print(' ');
+  SerialDebug.print(TCP_PORT);
+  SerialDebug.println("' to connect");
+}
+
+void loop() {
+  uint8_t i;
+  char buf[1024];
+  int bytesAvail, bytesIn;
+
+  //check if there are any new clients
+  if (tcpServer.hasClient()) {
+    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+      //find free/disconnected spot
+      if (!tcpServerClients[i] || !tcpServerClients[i].connected()) {
+        if (tcpServerClients[i]) tcpServerClients[i].stop();
+        tcpServerClients[i] = tcpServer.available();
+        SerialDebug.print("New client: "); SerialDebug.print(i);
+        continue;
+      }
+    }
+    //no free/disconnected spot so reject
+    WiFiClient tcpServerClient = tcpServer.available();
+    tcpServerClient.stop();
+  }
+
+  //check clients for data
+  for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+    if (tcpServerClients[i] && tcpServerClients[i].connected()) {
+      //get data from the telnet client and push it to the UART
+      while ((bytesAvail = tcpServerClients[i].available()) > 0) {
+        bytesIn = tcpServerClients[i].readBytes(buf, min(sizeof(buf), bytesAvail));
+        if (bytesIn > 0) {
+          SerialGPS.write(buf, bytesIn);
+          delay(0);
+        }
+      }
+    }
+  }
+
+  //check UART for data
+  while ((bytesAvail = SerialGPS.available()) > 0) {
+    bytesIn = SerialGPS.readBytes(buf, min(sizeof(buf), bytesAvail));
+    if (bytesIn > 0) {
+      //push UART data to all connected telnet clients
+      for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+        if (tcpServerClients[i] && tcpServerClients[i].connected()) {
+          tcpServerClients[i].write((uint8_t*)buf, bytesIn);
+          delay(0);
+        }
+      }
+    }
+  }
+}
---
+++ client.ino
@@ -0,0 +1,108 @@
+/*
+  Skyrc IMAX B6 - uart to serial port over esp8266 client
+  I used this scetch with nodemcu, because it got uart to serial port converter. 
+  Possible using with esp8266 + any ftdi convertor.
+  Remark: If you got bad uart signal :
+  remove resistor R4 https://github.com/neolead/skyrc-imax-b6-usb-uart-over-wifi-with-esp8266/raw/main/uart.jpg
+  thanks to : https://github.com/stawel/cheali-charger/issues/256
+  Created by neolead: janvarry@gmail.com nov.2020
+  thx to Tom Igoe 
+ */
+
+#include <ESP8266WiFi.h>
+#define MY_BLUE_LED_PIN 2
+
+
+const char* ssid     = "TCPUARTBridge";
+const char* password = "12345678";
+
+// Enter the IP address of the server you're connecting to:
+IPAddress server(192, 168, 4, 1);
+int port = 23;
+
+// Initialize the Wifi client library
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(MY_BLUE_LED_PIN, OUTPUT); 
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  //Serial.println();
+  //Serial.println();
+  //Serial.print("Connecting to ");
+  //Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+    delay(500);
+    digitalWrite(MY_BLUE_LED_PIN, HIGH);
+    //Serial.print(".");
+  }
+
+  //Serial.println("");
+  //Serial.println("WiFi connected");  
+  //Serial.println("IP address: ");
+  //Serial.println(WiFi.localIP());
+  // if you get a connection, report back via serial:
+  if (client.connect(server, port)) {
+    //Serial.println("connected to server");
+    digitalWrite(MY_BLUE_LED_PIN, HIGH);
+    delay(50);
+      }
+  else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection to server failed");
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+    delay(200);
+    digitalWrite(MY_BLUE_LED_PIN, HIGH);
+    delay(200);
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+  }
+}
+
+void loop()
+{
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+    delay(10);
+    digitalWrite(MY_BLUE_LED_PIN, HIGH);
+    delay(10);
+  }
+
+  // as long as there are bytes in the serial queue,
+  // read them and send them out the socket if it's open:
+  while (Serial.available() > 0) {
+    char inChar = Serial.read();
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+    delay(10);
+    digitalWrite(MY_BLUE_LED_PIN, HIGH);
+    delay(10);
+    if (client.connected()) {
+      client.print(inChar);
+    } else client.connect(server, port);
+    delay(10);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    //Serial.println("x");
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+    delay(10);
+    digitalWrite(MY_BLUE_LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(MY_BLUE_LED_PIN, LOW);
+    delay(10);
+   
+    client.stop();
+    delay(1000);
+  }
+}
